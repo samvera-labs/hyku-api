4d1438010c798b96238787f536fa0f00ead5fc05
Chris Colvard <chris.colvard@gmail.com>
2020-11-02 11:41:05 -0500

    Implement registrations endpoint

--- app/controllers/hyku/api/v1/registrations_controller.rb
+++ app/controllers/hyku/api/v1/registrations_controller.rb
@@ -1,9 +1,32 @@
 # frozen_string_literal: true
+require 'jwt'
+
 module Hyku
   module API
     module V1
       class RegistrationsController < BaseController
-        def create; end
+        def create
+          user = User.new(email: params['email'], password: params['password'],
+                          password_confirmation: params['password_confirmation'])
+
+          raise ActionController::BadRequest unless user.valid? && user.save
+
+          response.set_cookie(
+            :jwt,
+            value: generate_token(user), expires: 1.hour.from_now, path: '/', same_site: :none,
+            domain: ('.' + request.host), secure: true, httponly: true
+          )
+
+          render plain: "Please check your email at #{user.email} to complete your registration", status: 200
+        rescue ActionController::BadRequest
+          render json: { status: 401, code: 'Invalid credentials', message: user.errors.messages }
+        end
+
+        private
+
+          def generate_token(user)
+            JWT.encode({ user_id: user.id, exp: (Time.now.utc + 1.hour).to_i }, JWT_SECRET)
+          end
       end
     end
   end
---
+++ spec/requests/v1/registrations_spec.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+
+RSpec.describe Hyku::API::V1::RegistrationsController, type: :request do
+  let(:account) { create(:account) }
+  let(:email) { 'user@example.com' }
+  let(:password) { 'secure_password' }
+  let(:json_response) { JSON.parse(response.body) }
+  let(:jwt_cookie) { response.cookies.with_indifferent_access[:jwt] }
+
+  before do
+    WebMock.disable!
+    Apartment::Tenant.create(account.tenant)
+    Apartment::Tenant.switch(account.tenant) { Site.update(account: account) }
+  end
+
+  after do
+    WebMock.enable!
+    Apartment::Tenant.drop(account.tenant)
+  end
+
+  describe "/signup" do
+    it 'registers the user' do
+      post hyku_api.v1_tenant_users_signup_url(tenant_id: account.id), params: { email: email, password: password, password_confirmation: password }
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq "Please check your email at #{email} to complete your registration"
+      expect(jwt_cookie).to be_truthy
+    end
+
+    xcontext 'when email does not have correct domain' do
+      let(:domain) { 'example.com' }
+      let(:email) { 'user@test.com' }
+
+      it 'does not register the user' do
+        post hyku_api.v1_tenant_users_signup_url(tenant_id: account.id), params: { email: email, password: password, password_confirmation: password }
+
+        expect(response.status).to eq(200)
+        expect(json_response['status']).to eq(401)
+        expect(json_response['code']).to eq 'Invalid credentials'
+        expect(json_response['message']).to include("email" => ["Email must contain #{domain}"])
+        expect(jwt_cookie).to be_falsey
+      end
+    end
+
+    context 'when email is already registered' do
+      before do
+        post hyku_api.v1_tenant_users_signup_url(tenant_id: account.id), params: { email: email, password: password, password_confirmation: password }
+      end
+
+      it 'does not register the user' do
+        post hyku_api.v1_tenant_users_signup_url(tenant_id: account.id), params: { email: email, password: password, password_confirmation: password }
+
+        expect(response.status).to eq(200)
+        expect(json_response['status']).to eq(401)
+        expect(json_response['code']).to eq 'Invalid credentials'
+        expect(json_response['message']).to include("email" => ["has already been taken"])
+        expect(jwt_cookie).to be_falsey
+      end
+    end
+
+    context 'when password confirmation does not match password' do
+      it 'does not register the user' do
+        post hyku_api.v1_tenant_users_signup_url(tenant_id: account.id), params: { email: email, password: password, password_confirmation: 'different password' }
+
+        expect(response.status).to eq(200)
+        expect(json_response['status']).to eq(401)
+        expect(json_response['code']).to eq 'Invalid credentials'
+        expect(json_response['message']).to include("password_confirmation" => ["doesn't match Password"])
+        expect(jwt_cookie).to be_falsey
+      end
+    end
+  end
+end
