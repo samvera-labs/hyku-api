64a2a82fdb5f9ca1407835beceb67b1ebc773313
Eneko Taberna <eneko.taberna@gmail.com>
2021-03-05 12:21:31 +0100

    Fixes wrong Auth parsing regex, adds extra logs on auth headers

--- app/controllers/hyku/api/v1/base_controller.rb
+++ app/controllers/hyku/api/v1/base_controller.rb
@@ -12,17 +12,27 @@ module Hyku
 
           def load_token(name = :jwt)
             if request.headers['Authorization'].present?
-              login_from_token(request.authorization.match(/Bearer ([\w.]+)/).try(:[], 1))
+              logger.info "AUTH HEADER for #{name} found"
+              login_from_token(request.authorization.split(' ').last)
             elsif cookies[name].present?
+              logger.info "Cookie for #{name} found"
               login_from_token(cookies[name])
             end
           end
 
           def login_from_token(token)
+            logger.info "Login from token: #{token}"
             jwt = JWT.decode(token, JWT_SECRET)&.first&.with_indifferent_access
+            logger.info "VALID TOKEN"
             user = User.find_by(id: jwt['user_id']) if jwt
+            logger.info "User found: #{user.id}"
             sign_in user if user
-          rescue JWT::ExpiredSignature, JWT::DecodeError
+            logger.info "User logged in"
+          rescue JWT::ExpiredSignature => e
+            logger.warn "Expired Token: #{e.message}"
+            access_denied
+          rescue JWT::DecodeError => e
+            logger.warn "Decode error: #{e.message}"
             access_denied
           end
 
