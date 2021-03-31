26c514d21e10cd054cc75478c106f34b74eb4975
Eneko Taberna <eneko.taberna@gmail.com>
2021-03-31 11:45:34 +0200

    Appease Rubocop

--- app/presenters/concerns/hyku/api/work_show_presenter_behavior.rb
+++ app/presenters/concerns/hyku/api/work_show_presenter_behavior.rb
@@ -4,20 +4,19 @@ module Hyku
   module API
     module WorkShowPresenterBehavior
       extend ActiveSupport::Concern
-      
       # @return FileSetPresenter presenter for the thumbnail FileSets
       def thumbnail_presenter
         return nil if thumbnail_id.blank?
         @thumbnail_presenter ||=
-            begin
-              result = member_presenters_for([thumbnail_id]).first
-              return nil if result.try(:id) == id
-              if result.respond_to?(:thumbnail_presenter)
-                result.thumbnail_presenter
-              else
-                result
-              end
+          begin
+            result = member_presenters_for([thumbnail_id]).first
+            return nil if result.try(:id) == id
+            if result.respond_to?(:thumbnail_presenter)
+              result.thumbnail_presenter
+            else
+              result
             end
+          end
       end
     end
   end
