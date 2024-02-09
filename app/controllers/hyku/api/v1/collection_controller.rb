fb0898d6e8bd5e970106aed7bdf3ea176a885014
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2024-02-09 11:24:10 +0000

    REPO-2236 - Adding Parent collection info - bugfix

--- app/controllers/hyku/api/v1/collection_controller.rb
+++ app/controllers/hyku/api/v1/collection_controller.rb
@@ -51,7 +51,11 @@ module Hyku
 
         def collection_parent_collection_search_results
           @collection_parent_collection_search_results ||=
-            Hyrax::Collections::NestedCollectionQueryService.available_parent_collections(child: collection_presenter, scope: self, limit_to_id: nil)
+            Hyrax::Collections::NestedCollectionQueryService
+              .available_parent_collections(child: collection_presenter, scope: self, limit_to_id: nil)
+              .map do |result|
+              { "id" => result.id, "title_first" => result.title.first }
+            end.to_json
         end
 
         def authorized_child_collection_presenters
