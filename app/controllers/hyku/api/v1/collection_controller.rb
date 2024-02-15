196c360d4940865c3d3127066440ca298b0d7bc9
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2024-02-15 16:34:43 +0000

    REPO-2236 - Debugging code

--- app/controllers/hyku/api/v1/collection_controller.rb
+++ app/controllers/hyku/api/v1/collection_controller.rb
@@ -60,6 +60,7 @@ module Hyku
         end
 
         def parent_collection_search_results
+          puts "LOG_collection_presenter_at_line_63" + collection_presenter.inspect
           parent_from_nested_search = JSON.parse(available_parent_collections_data(collection: collection_presenter))
           puts "LOG_parent_from_nested_search" + parent_from_nested_search.inspect
           @parent_collection_search_results ||= parent_from_nested_search
