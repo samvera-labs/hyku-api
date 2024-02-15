d98b03504e986f75944b4361ed0d348057ffca74
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2024-02-15 13:13:27 +0000

    REPO-2236 - Getting parent info

--- app/controllers/hyku/api/v1/collection_controller.rb
+++ app/controllers/hyku/api/v1/collection_controller.rb
@@ -59,7 +59,7 @@ module Hyku
         end
 
         def parent_collection_search_results
-          parent_from_nested_search = JSON.parse(available_parent_collections_data(collection: collection_presenter))
+          parent_from_nested_search = collection_presenter.available_parent_collections(self)
           puts "LOG_parent_from_nested_search" + parent_from_nested_search.inspect
           @parent_collection_search_results ||= parent_from_nested_search
         end
