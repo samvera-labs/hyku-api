a50cd12220619171133e3052d0ee6d65625d9473
DanukaH <danuka.hettiarachchi@ubiquitypress.com>
2024-03-07 17:07:11 +0000

    REPO-2236 - New approach - getting solr parameters sorted

--- app/controllers/hyku/api/v1/collection_controller.rb
+++ app/controllers/hyku/api/v1/collection_controller.rb
@@ -7,7 +7,7 @@ module Hyku
 
         # self.search_builder Hyrax::CollectionSearchBuilder
         configure_blacklight do |config|
-          config.search_builder_class = Hyrax::CollectionMemberSearchBuilder
+          config.search_builder_class = Hyku::API::CollectionMemberSearchBuilder
         end
 
         def index
